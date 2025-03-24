de3a5ac587dc832a7c1eac35b822fd6b4e04276c
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-03-24 17:46:53 +0100

    [BONUS4]

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -19,6 +19,21 @@ function countVowels(word){
     return counter, vowelsInWord;
 }
 
+const countVowelsArrow = (word) => {
+    let counter = 0;
+    let vowelsInWord = [];
+    let vowels = ['a', 'e', 'i', 'o', 'u']
+    for(let i = 0; i < word.length; i++){
+        for(let j = 0; j < vowels.length; j++){
+            if (word[i] == (vowels[j])){
+                counter++;
+                vowelsInWord.push(vowels[j])
+            }
+        }
+    }
+    return counter, vowelsInWord;
+}
+
 // Invoca la funzione qui e stampa il risultato in console
 console.log(countVowels(word));
 
