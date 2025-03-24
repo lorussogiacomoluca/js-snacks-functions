548d0131d1c13b609bbfb382cd355818a309842d
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-03-24 17:03:16 +0100

    [UPD] 1.1 - Greet Function

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -1,7 +1,8 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
 const userName = 'Mario';
-
+const saluta = (nome) => `Ciao, ${nome}`;
+console.log(saluta(userName));
 
 // Dichiara la funzione qui.
 
