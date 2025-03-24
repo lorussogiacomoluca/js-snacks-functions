be5bae2c305b4b2bd309a7689d44635734fa4e94
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-03-24 17:41:50 +0100

    [FINAL] console log greetings

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -24,6 +24,6 @@ function sayHello(name){
 }
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(sayHello(name));
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
