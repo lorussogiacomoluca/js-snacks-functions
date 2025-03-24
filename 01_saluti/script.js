085aa7c3aba0565118348892db797f6c80d37b08
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-03-24 17:08:10 +0100

    [UPD] 1.2 Console log greetings

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -1,14 +1,13 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
 const userName = 'Mario';
-const saluta = (nome) => `Ciao, ${nome}`;
-console.log(saluta(userName));
+
 
 // Dichiara la funzione qui.
-
+const saluta = (nome) => `Ciao, ${nome}`;
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(saluta(userName));
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
